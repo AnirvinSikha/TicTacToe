7af97027d3eaa18de7dca4a3704630bfeaf45e27
Anirvin Sikha <anirvin@berkeley.edu>
2018-09-29 14:06:13 -0700

    initial commit

---
+++ Board.py
@@ -0,0 +1,8 @@
+class Board():
+    def __init__(self):
+        board = [None] * 9
+
+    def visualize(self):
+        return
+    def position_visuaize(self):
+        return
---
+++ Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, b, v):
+        self.board = b
+        self.val = v
+
+    def play(self):
+        position =  int(input("What position would you like to play? "))
+        while self.board[position] != None:
+            position = int(input("What position would you like to play? "))
+        
