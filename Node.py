b88109a89023e78439f92683ce4de1afa4952371
Anirvin Sikha <anirvin@berkeley.edu>
2018-11-03 16:20:18 -0700

    finished utility function

--- Node.py
+++ Node.py
@@ -13,13 +13,15 @@ class Node:
         test_grid = self.grid.copy()
         test_grid[self.position] = self.val
         if self.is_win(test_grid):
-            return float('inf')
+            util = float('inf')
+            return util
 
-        num_val = self.grid.count(self.val)
-        opp_val = self.grid.count(self.oppVal)
-
-        util += 10 * num_val + 5 * opp_val
-
+        neighbors = self.get_neighbors()
+        for i in neighbors:
+            if self.grid[i] == self.val:
+                util += 10
+            elif self.grid[i] == self.oppVal:
+                util += 5
         return util
 
     def is_win(self, grid, val):
@@ -32,3 +34,23 @@ class Node:
                 or b[0] == b[3] == b[6] == self.val \
                 or b[1] == b[4] == b[7] == self.val \
                 or b[2] == b[5] == b[8] == self.val)
+
+    def get_neighbors(self):
+        if self.position == 0:
+            return [1, 3]
+        elif self.position == 1:
+            return [0,2,4]
+        elif self.position == 2:
+            return [2,5]
+        elif self.position == 3:
+            return [0,4,6]
+        elif self.position == 4:
+            return [1,3,5,7]
+        elif self.position == 5:
+            return [2,4,8]
+        elif self.position == 6:
+            return [3,7]
+        elif self.position == 7:
+            return [4,6,8]
+        elif self.position == 8:
+            return [5,7]
